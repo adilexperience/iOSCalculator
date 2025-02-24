7e68aa0a11e456bf23f4ca5472cbbd0b79ecd57c
Muhammad Adil Mehmood <iamadilmalick@gmail.com>
2025-02-25 00:38:08 +0500

    Initial Commit

---
+++ iOSCalculatorTests/iOSCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCalculatorTests.swift
+//  iOSCalculatorTests
+//
+//  Created by Muhammad Adil Mehmood on 25/02/2025.
+//
+
+import Testing
+@testable import iOSCalculator
+
+struct iOSCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
